52597fc42c7d34d1c5ef233f20c39e2b1a97d753
cathyluong <cnluongg64@g.ucla.edu>
2026-07-27 22:05:03 -0700

    Co-authored-by: ouhnicole <ouhnicole@users.noreply.github.com> Co-authored-by: albertkun <albertkun@users.noreply.github.com>

---
+++ js/init.js
@@ -0,0 +1,91 @@
+// declare variables
+let mapOptions = {'centerLngLat': [-118.444,34.0709],'startingZoomLevel':5}
+
+const map = new maplibregl.Map({
+    container: 'map', // container ID
+    style: 'https://api.maptiler.com/maps/streets-v2-light/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
+    center: mapOptions.centerLngLat, // Starting position [lng, lat]
+    zoom: mapOptions.startingZoomLevel // Starting zoom level
+});
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button");
+    newButton.id = "button"+title;
+    newButton.innerHTML = title;
+    newButton.setAttribute("lat",lat);
+    newButton.setAttribute("lng",lng);
+    newButton.addEventListener('click', function(){
+        map.flyTo({
+            center: [lng,lat],
+        })
+    })
+    document.getElementById("contents").appendChild(newButton);
+}
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSycjDzRSIYoOrKh64s8viGDhLLU_Mgz82rkvO2YoEqqwScfeVXyno8y0Ghryh7GkNuhcmqOyj5jgYf/pub?output=csv"
+
+// When the map is fully loaded, start adding GeoJSON data
+map.on('load', function() {
+    Papa.parse(dataUrl, {
+        download: true, // Tells PapaParse to fetch the CSV data from the URL
+        header: true, // Assumes the first row of your CSV are column headers
+        complete: function(results) {
+            // Process the parsed data
+           console.log(results)
+        }
+    });
+//commit
+
+fetch('testimonies.geojson').then(
+    response => response.json()
+).then(
+    data => {
+        map.addSource('places', {
+            'type': 'geojson',
+            'data': data
+        });
+        map.addLayer({
+            'id': 'places',
+            'type': 'circle',
+            'source': 'places',
+            'paint': {
+                'circle-color': '#4264fb',
+                'circle-radius': 6,
+                'circle-stroke-width': 2,
+                'circle-stroke-color': '#ffffff'
+            }
+        });
+    }
+);
+
+});
+
+function processData(results){
+    //console.log(results) //for debugging: this can help us see if the results are what we want
+    results.features.forEach(feature => {
+        //console.log(feature) // for debugging: are we seeing each feature correctly?
+        // assumes your geojson has a "title" and "message" attribute
+        let coordinates = feature.geometry.coordinates;
+        let longitude = coordinates[0];
+        let latitude = coordinates[1];
+        let title = feature.properties.title;
+        let message = feature.properties.message;
+        addMarker(latitude,longitude,title,message);
+    });
+};
+function addMarker(data){
+    let popup_message;
+    let lng = data['lng'];
+    let lat = data['lat'];
+    if (data['Do you think UCLA has provided enough resources for students having experienced deportations?'] == "Yes"){
+        popup_message = `<h2>Students who think UCLA has provided enough resources</h2>`
+    }
+    else{
+        popup_message = `<h2>Students who think UCLA has not provided enough resources</h2>`
+    }
+    new maplibregl.Marker()
+        .setLngLat([lng, lat])
+        .setPopup(new maplibregl.Popup()
+            .setHTML(popup_message))
+        .addTo(map)
+    createButtons(lat,lng,data['Where did you get vaccinated?']);
+}
